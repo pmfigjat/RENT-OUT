70a95342286939e49bb90fef02cde51e556fe05c
Gesi <orgesymeri21@gmail.com>
2024-12-08 23:28:25 +0100

    cant properly validate add_product inputs

---
+++ js/add_product.js
@@ -0,0 +1,11 @@
+// const inputs = document.getElementsByTagName('input')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', (e) => {
+//     if (inputs.value == '' || inputs.value == null) {
+//         e.preventDefault();
+//         alert(" nuk duhet te jete e thate");
+//         return;
+//     }
+
+// });
