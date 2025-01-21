980a5fcd9e237336e14798b0963d62ddf8397a2f
Gesi <orgesymeri21@gmail.com>
2025-01-21 23:16:20 +0100

    post method aint workin at all for sum unkown reasonssssss

---
+++ includes/SignIn.inc.php
@@ -0,0 +1,41 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    echo "<h2>Form Submitted via POST</h2>";
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+} else {
+    echo "<h2>Form was NOT submitted via POST</h2>";
+    echo "Request method: " . $_SERVER["REQUEST_METHOD"];
+}
+// if(isset($_POST["submit"])) {
+//     $email = $_POST["email"];
+//     $name = $_POST["name"];
+//     $psw = $_POST["password"];
+
+//     require_once 'dbh.inc.php';
+//     require_once 'functions.inc.php';
+
+
+//     if (emptyInputSignup($email, $name, $psw) != false) {
+//         header("location: ../SignIn.php?error=emptyinput");
+//         exit();
+//     }
+//     // if (invalidname() != false) {
+//     //     header("location: ../SignIn.php?error=invalidname");
+//     //     exit();
+//     // }
+//     // if (invalidemail() != false) {
+//     //     header("location: ../SignIn.php?error=invalidname");
+//     //     exit();
+//     // }
+
+//     createUser($conn, $email, $name, $psw);
+
+// } 
+
+
+
+// else {
+//     header(("location: ../SignIn.php"));
+// }
---
+++ includes/functions.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+function emptyInputSignup($username, $email, $password,) {
+    if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
+        return true; // Returns true if any field is empty
+    } else {
+        return false; // Returns false if all fields are filled
+    }
+}
+
+function createUser($conn, $email, $name, $psw) {
+    $sql = "INSERT INTO users (name, email, psw) VALUES (?, ?, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+
+    if(!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../SignIn.php?error=stmtfailed");
+        exit();
+    }
+
+    $hashedpsw = password_hash($psw, PASSWORD_DEFAULT);
+    mysqli_stmt_bind_param($stmt, "ssss", $email, $name, $hashedpsw);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    header("location: ../home.php?error=none");
+        exit();
+}
